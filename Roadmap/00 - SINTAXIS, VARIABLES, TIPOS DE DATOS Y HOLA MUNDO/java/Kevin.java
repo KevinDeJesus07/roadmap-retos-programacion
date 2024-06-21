f72ffeb6e2e02d4058aa9ba2cb72df1735a761f7
KevinDeJesus07 <158528138+KevinDeJesus07@users.noreply.github.com>
2024-06-21 18:00:09 -0500

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Kevin.java
@@ -0,0 +1,32 @@
+package RetosDeProgramacion;
+
+public class Reto00 {
+
+    public static void main(String[] args) {
+        // https://www.oracle.com/java/
+
+        // Comentario de una Linea
+
+        /*
+         * Comentario de
+         * Varias
+         * Lineas
+         */
+
+        int miVariable = 0;
+        final int CONSTANTE = 1;
+
+        byte myByte = 0;
+        short myShort = 0;
+        int myInt = 0;
+        long myLong = 0;
+        float myFloat = 0.5f;
+        double myDouble = 0.33d;
+        boolean myBoolean = true;
+        String myString = "Texto";
+        char myChar = 'K';
+
+        System.out.print("¡Hola, Java!");
+    }
+
+}
